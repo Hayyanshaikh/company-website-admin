37783e6c5307ea4f8c50331de17d72e6d63c7c01
Hayyanshaikh <hayyanshaikh55@gamil.com>
2025-03-29 23:59:08 +0500

    Add feature management and image upload functionality; enhance layout

--- js/script.js
+++ js/script.js
@@ -20,7 +20,9 @@ $(document).ready(function () {
 
     let featureHTML = `
         <div class="flex items-center justify-start gap-2 bg-stone-100 border border-stone-300 py-2 px-4 rounded text-stone-800">
-          <i class="ph-bold ph-check-circle text-green-600"></i>
+          <button type="button" class="text-green-600 flex" id="removeListFeature">
+            <i class="ph-bold ph-check-circle text-lg"></i>
+          </button>
           <span>${featureText}</span>
           <button type="button" class="text-red-500 ml-auto removeFeature">
             <i class="ph-bold ph-trash text-lg"></i>
@@ -30,12 +32,43 @@ $(document).ready(function () {
 
     // Agar "No features added" message hai, usko remove karo
     $("#featureList p").remove();
-    $("#featureList").append(featureHTML);
+    $("#featureList").prepend(featureHTML);
     $("#featureInput").val(""); // Clear input after adding
   });
 
   $(document).on("click", ".removeFeature", function () {
     $(this).parent().remove();
     updateFeatureMessage();
+  });
+
+  $(document).on("click", "#removeListFeature", function () {
+    let icon = $(this).find("i");
+
+    if (icon.hasClass("ph-check-circle")) {
+      icon
+        .removeClass("ph-check-circle text-green-600 checked")
+        .addClass("ph-x-circle text-red-600 unchecked");
+    } else {
+      icon
+        .removeClass("ph-x-circle text-red-600 unchecked")
+        .addClass("ph-check-circle text-green-600 checked");
+    }
+  });
+
+  $(".imageUpload").on("change", function (event) {
+    const file = event.target.files[0]; // Selected file ko get karo
+    if (file) {
+      const reader = new FileReader();
+      const previewContainer = $(this).siblings(".previewContainer");
+
+      reader.onload = function (e) {
+        previewContainer.html(`
+                    <img src="${e.target.result}" alt="Uploaded Image" 
+                         class="w-32 h-32 object-cover rounded border border-gray-300 shadow">
+                `);
+      };
+
+      reader.readAsDataURL(file);
+    }
   });
 });
